5ef8aa96b45fc12b0a613cbd8c02534380789739
Arpan-Dey-Web <arpandey.web@gmail.com>
2025-07-24 12:57:43 +0600

    basic routes and simple navbar created

---
+++ src/Components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const Home = () => {
+  return (
+    <div className="bg-[#EFF5E1]">
+      <div className="w-11/12 mx-auto max-w-7xl">
+        <Navbar></Navbar>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
